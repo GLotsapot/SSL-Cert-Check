26fe130c669eaf8390c32c8fe8e6f309767cd8dd
GLotsapot @Work <GLotsapot@users.noreply.github.com>
2017-11-13 16:19:39 -0500

    Initial Working Prototype
    This is the first commit with a basic working program.
    Additional features to be added at a later time

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace SSLCertCheck
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string[] sites;
+
+			try {
+				sites = LoadSites();
+			} catch (Exception ex) {
+				Console.WriteLine ("There was an issue trying to load the sites listing - {0}", ex.Message);
+				return;
+			}
+
+			foreach (var site in sites) {
+				Console.WriteLine ("Checking: {0}", site);
+				var siteCheck = new SitesInfo(site);
+				Console.WriteLine ("-- Expiration: {0}", siteCheck.Certificate.GetExpirationDateString ());
+
+			}
+		}
+
+		public static string[] LoadSites()
+		{
+			string sitesListFile = "./certlist.txt";
+			if(!File.Exists(sitesListFile)){
+				Console.WriteLine ("The file {0} with a list of URLs did not exist. Creating a blank one for your convinience.", sitesListFile);
+				try {
+					System.IO.File.CreateText (sitesListFile);
+					Console.WriteLine("The file was successfully created");
+				} catch (Exception ex) {
+					Console.WriteLine ("Failed to create file: {0}", ex.Message);
+				}
+
+			}
+
+			var sitesList = System.IO.File.ReadAllLines(sitesListFile);
+			return sitesList;
+		}
+
+	}
+}
---
+++ SitesInfo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+
+namespace SSLCertCheck
+{
+	public class SitesInfo
+	{
+		public SitesInfo(string url) : this(url,443)
+		{
+		}
+
+		public SitesInfo(string url, int port)
+		{
+			this.Url = url;
+			this.Port = port;
+
+			CheckCert ();
+		}
+
+		#region Properties
+
+		public string Url {
+			get;
+			set;
+		}
+
+		public int Port {
+			get;
+			set;
+		}
+
+		public X509Certificate Certificate {
+			get;
+			private set;
+		}
+
+		#endregion
+
+		#region Methods
+
+		private void CheckCert()
+		{
+			ServicePointManager.ServerCertificateValidationCallback += ServerCertificateValidationCallback;
+
+			var request = WebRequest.Create(this.Url);
+			request.GetResponse();
+		}
+
+		private bool ServerCertificateValidationCallback(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+		{
+			this.Certificate = certificate;
+
+			return true;
+		}
+
+		#endregion
+	}
+}
+
