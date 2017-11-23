bcf1ca827903b42005be0cfed7d92a1d66d46ec9
Groda Lotsapot <glotsapot@users.noreply.github.com>
2017-11-23 18:02:05 -0500

    Logging and Events
    Added loging to SitesInfo class
    Removed event ServerCertificateValidationCallback after its used as it was causing it to fire more than once (funny enough the logging is what helped determine this)

--- SitesInfo.cs
+++ SitesInfo.cs
@@ -20,20 +20,22 @@ namespace SSLCertCheck
             this.Url = url;
         }
 
-		#endregion
+        #endregion
 
 
-		#region Fields
+        #region Fields
 
-		#endregion
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        #endregion
 
 
-		#region Properties
+        #region Properties
 
         /// <summary>
         /// Full url of the website to check
         /// </summary>
-		public string Url { get; set; }
+        public string Url { get; set; }
 
         /// <summary>
         /// Expiration date of the certificate
@@ -57,18 +59,23 @@ namespace SSLCertCheck
         /// </summary>
         public void CheckCert()
 		{
-			ServicePointManager.ServerCertificateValidationCallback += ServerCertificateValidationCallback;
+            log.Debug("CheckCert: Requesting website");
+
+            ServicePointManager.ServerCertificateValidationCallback += ServerCertificateValidationCallback;
 
             var request = (HttpWebRequest)WebRequest.Create(this.Url);
             request.AllowAutoRedirect = false;
             request.GetResponse();
-		}
+
+            ServicePointManager.ServerCertificateValidationCallback -= ServerCertificateValidationCallback;
+        }
 
         /// <summary>
         /// Catches the SSL certificate check when a WebRequest is made
         /// </summary>
 		private bool ServerCertificateValidationCallback(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
 		{
+            log.Debug(String.Format("ServerCertificateValidationCallback: Got a cert for {0}", certificate.Subject));
             var newCert = (X509Certificate2)certificate;
             this.Certificate = newCert;
 
